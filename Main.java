41966cc395cb51a802516346e032a068c1f72c2b
Ala' Qureshi <alaaliqureshi@gmail.com>
2021-02-10 16:52:15 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+class Main { 
+public static void main(String[] args) { 
+int grade = 69;
+if (grade == 90 || grade > 90)
+{
+  System.out.println("A= \"Aces\"");
+}
+else if (grade == 81 || grade == 82 || grade == 83 || grade == 84 || grade == 85 || grade == 86 || grade == 87 || grade == 88 || grade == 89 || grade == 80)
+{
+  System.out.println("B= \"You should have done better\"");
+}
+
+else 
+{
+  System.out.println("Anything under a B= \"You are not aloud to live in my house because you got lower than a B\"");
+}
+}
+
+} 
+
+
