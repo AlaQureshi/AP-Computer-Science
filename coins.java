1d67ceabfd5fd074fe4b6ef01af9534091fcb5b8
AlaQureshi <78869222+AlaQureshi@users.noreply.github.com>
2021-02-25 19:28:15 -0500

    added coins and food order assignment

--- coins.java
+++ coins.java
@@ -3,12 +3,12 @@ public class coins {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
         System.out.println("Enter amount in cents: ");
-        double cents = read.nextInt();
+        int cents = read.nextInt();
 
-        double quarters = cents/25;
-        double dimes = cents%25/10;
-        double nickels = dimes%25%10/5;
-        double nickelsLeft = cents%5;
+        int quarters = cents/25;
+        int dimes = cents%25/10;
+        int nickels = dimes%25%10/5;
+        int nickelsLeft = cents%5;
 
         System.out.println("Quarters: " + quarters);
         System.out.println("Dimes: " + dimes);
